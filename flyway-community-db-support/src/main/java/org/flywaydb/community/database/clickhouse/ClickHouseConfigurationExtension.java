6772197deb7aec2bdabfa2b5d192cf95efe3bddd
Dmitry Mityushin <22773791+Mityushin@users.noreply.github.com>
2023-02-27 17:27:03 +0300

    porting https://github.com/sazonov/flyway/tree/clickhouse-support to flyway-8.5.13

---
+++ flyway-community-db-support/src/main/java/org/flywaydb/community/database/clickhouse/ClickHouseConfigurationExtension.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) Red Gate Software Ltd 2010-2022
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.community.database.clickhouse;
+
+import lombok.Getter;
+import org.flywaydb.core.extensibility.ConfigurationExtension;
+
+import java.util.Map;
+
+@Getter
+public class ClickHouseConfigurationExtension implements ConfigurationExtension {
+    private static final String CLUSTER_NAME = "flyway.clickhouse.clusterName";
+
+    private String clusterName;
+
+    @Override
+    public void extractParametersFromConfiguration(Map<String, String> configuration) {
+        clusterName = configuration.get(CLUSTER_NAME);
+        configuration.remove(CLUSTER_NAME);
+    }
+
+    @Override
+    public String getConfigurationParameterFromEnvironmentVariable(String environmentVariable) {
+        if ("FLYWAY_CLICKHOUSE_CLUSTER_NAME".equals(environmentVariable)) {
+            return CLUSTER_NAME;
+        }
+        return null;
+    }
+}
